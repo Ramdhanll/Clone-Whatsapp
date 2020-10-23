6c9aeae12e1295a1e59a0e4049ace9212dc8d081
ramdhanll <bscdhani30@gmail.com>
2020-10-23 22:39:38 +0700

    first commit

---
+++ client/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+   apiKey: "AIzaSyDrbYEwRedT5JXHox2-0sfnxsiRy4W4OHE",
+   authDomain: "mern-clone-whatsapp.firebaseapp.com",
+   databaseURL: "https://mern-clone-whatsapp.firebaseio.com",
+   projectId: "mern-clone-whatsapp",
+   storageBucket: "mern-clone-whatsapp.appspot.com",
+   messagingSenderId: "600338016213",
+   appId: "1:600338016213:web:62344be7aaf40640e47f5c"
+};
